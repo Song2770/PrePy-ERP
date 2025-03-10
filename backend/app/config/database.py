4b2452afec1d8b57b93eca350164b72452fed5c1
Rivensbane <oncwnuIUJGxC6xoqv55_zAUxGvDY@git.weixin.qq.com>
2025-03-10 09:27:54 +0800

    fix

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -15,7 +15,7 @@ DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "postgres")
 
 # 添加 SSL、超时配置和 UTF-8 编码设置
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}[?connect_timeout=10&sslmode=prefer&client_encoding=utf8]"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?connect_timeout=10&sslmode=prefer&client_encoding=utf8"
 
 # Create SQLAlchemy engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
